fd55f74790bf54d1341e169aef5f6ef117979a3a
Vicky Juan <vicky37@uw.edu>
2017-01-20 21:07:44 -0800

    Finished

---
+++ app/src/main/java/edu/washington/vicky37/biggobutton/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.washington.vicky37.biggobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private int ButtonCounter=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButtonActivity();
+    }
+
+    public void ButtonActivity(){
+        final Button GiantButton = (Button) findViewById(R.id.button);
+        GiantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ButtonCounter++;
+                GiantButton.setText("You have pushed me "+ButtonCounter+" times");
+            }
+        });
+    }
+}
